fbe31c04accd8210bd47df78234eea4c58941a65
Teddy <julan.teddy@gmail.com>
2023-11-23 21:28:15 +0100

    Je travaille sur la partie Porfolio mais c'est encore flou au niveau du modal.

---
+++ script.js
@@ -0,0 +1,16 @@
+$document.ready(function() {
+$ (".button").click(function(){
+    var value =$(this).attr("data-filter");
+    if(value=="all"){
+        $(".filter").show("100");
+    }
+    else{
+        $(".filter").not("."+value).hide("1000");
+        $(".filter").filter("."+value).show("1000");
+
+    }("ul .button").click (function(){
+    $(this).addClass('active').siblings() .removeClass('active');
+
+})
+})
+})
